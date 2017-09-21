9fedab720f4d6b038776bff721b42475de0271a8
maerlie <maliedeyouciang@163.com>
2017-09-21 18:10:39 +0800

    修改

--- src/test/test.java
+++ src/test/test.java
@@ -1,9 +1,11 @@
 package test;
 
+import java.util.Date;
+
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(new Date());
 
 	}
 
